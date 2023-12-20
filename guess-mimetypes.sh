99322358099baf4ee37e281411383e78dcef5154
david raistrick <>
2023-12-20 16:08:00 -0500

    try stuff.

--- guess-mimetypes.sh
+++ guess-mimetypes.sh
@@ -14,7 +14,7 @@ find "$root_directory" -type d -name '.git' -prune -o -type f ! -name "*.*" -pri
 
     # Generate vercel.json headers entry
     echo "    {"
-    echo "      \"source\": \"/$relative_path\","
+    echo "      \"source\": \"/$relative_path/\","
     echo "      \"headers\": [{ \"key\": \"Content-Type\", \"value\": \"$mime_type\" }]"
     echo "    },"
 done
